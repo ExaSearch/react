43afc6a3bf89e7796e14f4a18d3ffc5384fe0f21
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2021-04-02 17:10:51 -0400

    init

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import { useCallback, useRef, useEffect, useState } from "react";
+import './App.css';
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>welcome to exa search</h1>
+    </div>
+  );
+}
+
+export default App;
